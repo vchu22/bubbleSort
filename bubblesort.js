5c13a1178c095de309f8d9d2153c59b554ad47f0
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2019-01-18 16:58:49 -0500

    commit

--- bubblesort.js
+++ bubblesort.js
@@ -1,7 +1,16 @@
 function bubbleSort(array) {
   /* your code here */
-  let sortedArr = [];
+  let sortedArr = array;
   if (array.length <= 1) return array;
   else {
+    for (let i = 0; i < array.length; i++) {
+      for (let j = i; j < array.length - 1; j++) {
+        if (array[j] > [j + 1]) {
+          let temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+        }
+      }
+    }
   }
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -2,4 +2,8 @@ describe('Bubble Sort', function() {
   it('handles an empty array', function() {
     expect(bubbleSort([])).toEqual([]);
   });
+  it('make sure it correctly sorted', function() {
+    let input = [3, 2, 7, 8, 1, 4, 9];
+    expect(bubbleSort(input)).toEqual([1, 2, 3, 4, 7, 8, 9]);
+  });
 });
